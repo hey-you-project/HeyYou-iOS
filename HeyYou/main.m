ee8f2b1bcd4160e47b3137a3604f92286c7ccd37
Will Richman <wrichman@gmail.com>
2014-11-17 09:54:58 -0800

    Initial Commit

---
+++ HeyYou/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HeyYou
+//
+//  Created by William Richman on 11/17/14.
+//  Copyright (c) 2014 Hey You!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
