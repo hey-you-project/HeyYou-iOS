c6be0f65c3bd70a3e681f241e21ff39e1c66787c
Will Richman <wrichman@gmail.com>
2014-11-17 10:32:50 -0800

    Created Dot object.

---
+++ HeyYou/Dot.h
@@ -0,0 +1,23 @@
+//
+//  Dot.h
+//  HeyYou
+//
+//  Created by William Richman on 11/17/14.
+//  Copyright (c) 2014 Hey You!. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface Dot : NSObject
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *body;
+@property (nonatomic) CLLocationCoordinate2D location;
+@property (nonatomic, strong) NSDate *timestamp;
+@property (nonatomic, strong) NSString *identifier;
+@property (nonatomic, strong) NSString *color;
+@property (nonatomic) NSInteger stars;
+@property (nonatomic, strong) NSMutableArray *comments;
+
+@end
